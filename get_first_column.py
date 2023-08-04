97bf8ec830495e9dc9a0d0e2e2a318b6e96add99
habibulloabdullayev <habibulloabdullayev02@gmail.com>
2023-08-04 11:03:34 +0500

    get_first_column

--- get_first_column.py
+++ get_first_column.py
@@ -6,6 +6,13 @@ def get_first_column(data):
     Return:
         list: First column.
     """
-    return
+    rows = data.split('\n')
+    countries = []
+    for row in rows:
+        user = row.split(',')
+        countries.append(user[0])
+    return countries
     
 # Read the csv file
+f = open('data.csv')
+print(get_first_column(f.read()))
